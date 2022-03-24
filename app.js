e91bc59fb9148b1dacece3dc57f4d5560a58028f
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-03-24 22:41:46 +0100

    added main menu

--- app.js
+++ app.js
@@ -56,39 +56,58 @@ console.log("With this you can download packages and if nessecary install them."
 // Log a minus line
 console.log("-".repeat(Math.min(process.stdout.columns, 65)));
 
-console.log("What package do you want to download/install?");
-console.log("1: Avatargenerator");
-console.log("2: Stream Overlay");
+// Ask what to do
+console.log("What do you want to do?");
+console.log("1: Download a package");
+console.log("2: Settings");
+rl.question("Waiting for input... (1-2)\n> ", (todo) => {
 
+    // Switch the todo
+    switch (todo) {
 
-// Ask which program should be installed
-rl.question('Waiting for input... (1-2)\n> ', (package) => {
+        // Case 1: Download a package
+        case "1":
 
-    // Check if the package if the chosen package number is valid (var links)
-    if (package in links) {
+            // Clear the console
+            console.clear();
 
-        // Status message
-        console.log("Downloading package...");
+            // Ask which program should be installed
+            console.log("What package do you want to download/install?");
+            console.log("1: Avatargenerator");
+            console.log("2: Stream Overlay");
+            rl.question('Waiting for input... (1-2)\n> ', (package) => {
 
-        // Download the package
-        download(links[package]);
+                // Check if the package if the chosen package number is valid (var links)
+                if (package in links) {
 
-        // Status message
-        console.log("Download complete!");
-        console.log("Installing package...");
+                    // Status message
+                    console.log("Downloading package...");
 
-        // Close the readline interface
-        rl.close();
-    } else {
+                    // Download the package
+                    download(links[package]);
 
-        // If the package is not valid
-        console.log("Invalid package!");
-        console.log("Please try again!");
-        rl.close();
-        // Restart the program
-        process.exit(0);
+                    // Status message
+                    console.log("Download complete!");
+                    console.log("Installing package...");
+
+                    // Close the readline interface
+                    rl.close();
+                } else {
+
+                    // If the package is not valid
+                    console.log("Invalid package!");
+                    console.log("Please try again!");
+                    rl.close();
+                    // Restart the program
+                    process.exit(0);
+                }
+
+                // Close the readline interface
+                rl.close();
+            });
+
+            // Break the switch
+            break;
+
     }
-
-    // Close the readline interface
-    rl.close();
 });
