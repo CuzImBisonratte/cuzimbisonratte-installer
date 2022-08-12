6630f48d18c1af551a9548e1c2538719caedcb58
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-08-13 00:21:03 +0200

    Add readline module

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // Import modules
+const readline = require('readline');
 const gh_links = require("./modules/github_link_creation.js");
 const file_download = require("./modules/download_file.js");
 const messages = require("./modules/message_builder.js");
 
+// Create the readline interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
 // Send intro message
 messages.sendIntro();
+
+// Close readline interface
+rl.close();
