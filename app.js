4a460bf131576ab113b1eb8b9cbc70bed0dc7d4b
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-08-12 21:23:54 +0200

    Add function to download a file

--- app.js
+++ app.js
@@ -15,3 +15,17 @@ function getGithubSourcecodeLink(repo, tag) {
     var SourceCodeLink = "https://github.com/" + repo + "/archive/refs/tags/" + tag + ".zip";
     return SourceCodeLink;
 }
+
+// Function to download a file from a given url to a specific file name
+async function downloadFile(link, filename = "download") {
+    const response = await fetch(link);
+    const body = await response.body;
+    const filepath = "./" + filename;
+    const download_write_stream = fs.createWriteStream(filepath);
+    const stream = new WritableStream({
+        write(chunk) {
+            download_write_stream.write(chunk);
+        },
+    });
+    await body.pipeTo(stream);
+}
