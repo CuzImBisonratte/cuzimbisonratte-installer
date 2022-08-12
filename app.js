a3362ea1aa610178b9d817ae8db023cd5b4543b6
CuzImBisonratte <cuzimbisonratte@gmail.com>
2022-08-12 22:39:21 +0200

    seperated github functions to module files

--- app.js
+++ app.js
@@ -1,26 +1,6 @@
 // Import modules
 const fs = require('fs');
-
-// Function to get Latest Github Sourcecode
-async function getLatestGithubSourcecodeLink(repo) {
-    var github_api_link = "https://api.github.com/repos/" + repo + "/releases/latest";
-    var response = await fetch(github_api_link);
-    var body = await response.json();
-    var SourceCodeLink = "https://github.com/" + repo + "/archive/refs/tags/" + body.tag_name + ".zip";
-    return SourceCodeLink;
-}
-
-// Function to get Github Sourcecode from a specific tag
-function getGithubSourcecodeLink(repo, tag) {
-    var SourceCodeLink = "https://github.com/" + repo + "/archive/refs/tags/" + tag + ".zip";
-    return SourceCodeLink;
-}
-
-// Function to get Github Release Codefiles
-function getGithubReleaseFile(repo, tag, filename) {
-    var ReleaseFileLink = "https://github.com/" + repo + "/releases/download/" + tag + "/" + filename;
-    return ReleaseFileLink;
-}
+const gh_links = require("./modules/github_link_creation.js")
 
 // Function to download a file from a given url to a specific file name
 async function downloadFile(link, filename = "download") {
